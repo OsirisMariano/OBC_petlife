5ed9d99b4a9afb737967b0c518e0f936746e8fed
Osiris Mariano <oslimacontato@gmail.com>
2024-04-23 21:34:42 -0300

    Finalizando a seção de dúvidas com CSS e JavaScript

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var elementosDuvida = document.querySelectorAll('.duvida')
 
 elementosDuvida.forEach(function(duvida){
   duvida.addEventListener('click', function () {
-    duvida.classList.add('fundo-verde')
+    duvida.classList.toggle('ativa')
   })
 })
